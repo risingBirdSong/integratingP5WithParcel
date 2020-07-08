8eead9b05382d8f4157afd5d295083bcaf525557
risingbirdsong <heregoingthere@outlook.com>
2020-07-08 10:33:35 -0700

    parcel wasnt working and this is a correct answer from a stack overflow question i asked on how to get it to work :)

---
+++ index.js
@@ -0,0 +1,61 @@
+//sketch.js
+"use strict";
+// took this stack overflow suggestion and it worked :) thanks phhu !
+// https://stackoverflow.com/questions/62786847/how-can-i-use-parcel-js-to-run-p5-js-code/62799251#62799251
+let stars = [];
+
+window.speed = undefined;
+
+window.setup = function setup() {
+  createCanvas(600, 600);
+  for (let i = 0; i < 800; i++) {
+    stars[i] = new Star();
+  }
+};
+
+window.draw = function draw() {
+  speed = map(mouseX, 0, width, 0, 50);
+  background(0);
+  translate(width / 2, height / 2);
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
+};
+
+//Star.js
+window.Star = function Star() {
+  this.x = random(-width, width);
+  this.y = random(-height, height);
+  this.z = random(width);
+  this.pz = this.z;
+
+  this.update = function () {
+    this.z = this.z - speed;
+    if (this.z < 1) {
+      this.z = width;
+      this.x = random(-width, width);
+      this.y = random(-height, height);
+      this.pz = this.z;
+    }
+  };
+
+  this.show = function () {
+    fill(255);
+    noStroke();
+
+    var sx = map(this.x / this.z, 0, 1, 0, width);
+    var sy = map(this.y / this.z, 0, 1, 0, height);
+
+    var r = map(this.z, 0, width, 16, 0);
+    ellipse(sx, sy, r, r);
+
+    var px = map(this.x / this.pz, 0, 1, 0, width);
+    var py = map(this.y / this.pz, 0, 1, 0, height);
+
+    this.pz = this.z;
+
+    stroke(255);
+    line(px, py, sx, sy);
+  };
+};
